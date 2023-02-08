5ac187ae8bcc7f37e1d7e50b674ee23491b00332
Sunday Omolewu <omolewu12@gmail.com>
2023-02-08 12:36:53 +0100

    include public folder

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path')
 const express = require('express');
 const routePage = require('./routes/pages')
 const pageRouter = require('./routes/pages');
 const bodyParser = require('body-parser');
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(routePage);
 app.listen(3001)
--- routes/pages.js
+++ routes/pages.js
@@ -4,7 +4,6 @@ const path = require('path')
   const router = express.Router();
 
 router.get('/', (req, res, next) =>{
- 
  res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
 
 })
